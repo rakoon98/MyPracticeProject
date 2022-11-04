5e40cfb4353db67fab3b04f764149741baa4eeb6
fpin9 <kun93686@gmail.com>
2022-11-04 16:08:14 +0900

    Compose : navigation Content Padding

--- app/src/main/java/com/example/test/compose/ui/ComposeApp.kt
+++ app/src/main/java/com/example/test/compose/ui/ComposeApp.kt
@@ -1,11 +1,13 @@
 package com.example.test.compose.ui
 
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.navigation.compose.rememberNavController
 import com.example.test.compose.model.nav.BottomNavBuild
 import com.example.test.compose.model.nav.ComposeNavigation
@@ -20,7 +22,11 @@ fun ComposeApp() {
         Scaffold(
             bottomBar = { BottomNavBuild(navController) }
         ) { sPadding ->
-            ComposeNavigation(navController)
+            Box(modifier = Modifier.padding(
+                PaddingValues(0.dp, 0.dp, 0.dp, sPadding.calculateBottomPadding())
+            )) {
+                ComposeNavigation(navController)
+            }
         }
     }
 }
